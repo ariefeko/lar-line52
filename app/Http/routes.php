39640031658dc141611c0ee589954fa0a2b4b8bc
Arief Eko <riff.sense@gmail.com>
2018-03-15 15:03:04 +0700

    prepare webhook

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(array('prefix' => 'api'), function () {
+    Route::get('get-message', function (Request $request) {
+        logger("message request : ", $request->all());
+    });
+
+    Route::post('get-message', ['as' => 'line.bot.message', 'uses' => 'GetMessageController@getMessage']);
 });
