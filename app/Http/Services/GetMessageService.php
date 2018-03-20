73191c027049441ff5e63bc78a17e43a152b1b40
Arief Eko <riff.sense@gmail.com>
2018-03-20 07:40:12 +0700

    update

--- app/Http/Services/GetMessageService.php
+++ app/Http/Services/GetMessageService.php
@@ -18,13 +18,14 @@ class GetMessageService
 
     public function replySend($formData)
     {
+        $data = json_decode(json_encode(buttonObj()));//dd($data);
+
         // dd($formData);
         $replyToken = $formData['events']['0']['replyToken'];
         $this->client = new CurlHTTPClient(env('LINE_BOT_ACCESS_TOKEN'));
         $this->bot = new LINEBot($this->client, ['channelSecret' => env('LINE_BOT_SECRET')]);
-        dd($replyToken);
-
-        $response = $this->bot->replyMessage($replyToken, 'hello!');
+        $response = $this->bot->replyText($replyToken, "Hello!");
+        // dd($response);
 
         if ($response->isSucceeded()) {
             return;
